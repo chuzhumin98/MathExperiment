3605997a9ea45ca88be35f1d7c3b81748e051981
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-08 01:22:08 +0800

    add for iterative method

--- EXP 5-6/Exp5Problem9/Exp5Problem9.m
+++ EXP 5-6/Exp5Problem9/Exp5Problem9.m
@@ -10,22 +10,18 @@ A(1,:) = A(1,:) + bi; %设置第一行的值
 A(2:n,1:n-1) = A(2:n,1:n-1) + diag(si); %设置下次对角线
 b = [0; hi(1:n-1)'];
 %% 采用直接法求解方程组
-x1 = A\b;
-%% 尝试采用Jacobi迭代法求解
+x0 = A\b;
+%% 设置迭代法所需的常量
 epsilon = 1e-6; %设置迭代法收敛的阈值
+itermax = 1000; %设置最大的迭代次数
+%% 尝试采用Jacobi迭代法求解
 D = diag(diag(A)); %对角阵
-U = triu(A, 1); %去对角上三角块
-L = tril(A, -1); %去对角下三角块
+U = -triu(A, 1); %去对角上三角块
+L = -tril(A, -1); %去对角下三角块
 B1 = D^(-1)*(L+U);
 f1 = D^(-1)*b;
-rou1 = max(abs(eig(B1)));
-if (rou1 < 1) %收敛时则考虑使用Jacobi迭代法
-    
-end
+[x1, iter1, rou1] = Iterative(B1, f1, itermax, epsilon);
 %% 尝试采用Guass-Seideil迭代法求解
 B2 = (D-L)^(-1)*U;
 f2 = (D-L)^(-1)*b;
-rou2 = max(abs(eig(B2)));
-if (rou2 < 1) %收敛时则考虑使用Jacobi迭代法
-    
-end
+[x2, iter2, rou2] = Iterative(B2, f2, itermax, epsilon);
