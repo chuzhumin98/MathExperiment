7c8ff4757f4542661b26acf2bb76d42fdb3e7d60
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-08 20:50:39 +0800

    add for more parameters

--- Exp 5-6/Exp6Problem6/getRandom.m
+++ Exp 5-6/Exp6Problem6/getRandom.m
@@ -4,5 +4,5 @@ function XT = getRandom(n)
     xinit = rand(1,n); %生成各组分的随机数
     x = xinit / sum(xinit); %将各组分之间进行归一化
     XT(1:n-1) = x(1:n-1);
-    XT(n) = 50 + 250 * rand(); %温度取-50至200间的随机数
+    XT(n) = -50 + 350 * rand(); %温度取-50至200间的随机数
 end
