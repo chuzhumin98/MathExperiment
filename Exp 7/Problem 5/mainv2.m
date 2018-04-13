20aabdb5f2baf4f8e7f206bac6a748a393cdcc10
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-13 10:51:55 +0800

    end for problem 5

--- Exp 7/Problem 5/mainv2.m
+++ Exp 7/Problem 5/mainv2.m
@@ -4,9 +4,9 @@ n = 25;
 opt = optimset('fminunc');
 opt = optimset(opt, 'maxiter',1e4);
 fmin = 1.0000;
-for i = 1:100
-    X0 = 2*rand(n-1, 2)-1;
-    [x,f,e,out] = fminunc(@atomposition, X0, opt, data(:,1), data(:,2), data(:,3));
+for i = 1:10
+    X0 = 2*rand(2*n-3,1)-1;
+    [x,f,e,out] = fminunc(@atompositionv2, X0, opt, data(:,1), data(:,2), data(:,3));
     if (fmin > f)
         fmin = f;
         Xmin = x;
