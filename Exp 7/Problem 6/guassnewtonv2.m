9ea8ee532ceb5db4bed6b36273aab9f187417c51
chuzhumin98 <chuzhumin98@gmail.com>
2018-04-13 13:45:46 +0800

    complete for problem 6

--- Exp 7/Problem 6/guassnewtonv2.m
+++ Exp 7/Problem 6/guassnewtonv2.m
@@ -1,26 +1,24 @@
 %%  Gauss-Newton method  
-
-function [x, F, k] = guassnewtonv2(t, y, x0, error, itermax)
+function [x, norm, k] = guassnewtonv2(t, y, x0, error, itermax)
     format short;   
-    x = x0; 
-    f = @(x1,x2,x3,x4,x5) x1 + x2 * exp(-x4*t) + x3 * exp(-x5*t) - y;   
+    x = x0;  
     J = [];  %初点数值带入表达式
     for i = 1:length(t)
         J = [J; 1, exp(-x(4)*t(i)), exp(-x(5)*t(i)), -x(2)*t(i)*exp(-x(4)*t(i)), -x(3)*t(i)*exp(-x(5)*t(i))];
     end       
-    F=x(1) + x(2) * exp(-x(4)*t) + x(3) * exp(-x(5)*t) - y;  
-    k=0;               %迭代次数  
-    while (sum((J'*F).^2))^(1/2)>error  %判断停机与否, 这里相当于求梯度的模  
-        d=-inv(J'*J)*J'*F;          %搜索方向  
-        x=x+d;                      %新的迭代点  
+    F = x(1) + x(2) * exp(-x(4)*t) + x(3) * exp(-x(5)*t) - y;  
+    k = 0;               %迭代次数  
+    while (sum((J'*F).^2))^(1/2) > error  %判断停机与否, 这里相当于求梯度的模  
+        d = -inv(J'*J) * J' * F;          %搜索方向  
+        x = x + d;                      %新的迭代点  
         J = []; %新的迭代点数值带入表达式 
         for i = 1:length(t)
             J = [J; 1, exp(-x(4)*t(i)), exp(-x(5)*t(i)), -x(2)*t(i)*exp(-x(4)*t(i)), -x(3)*t(i)*exp(-x(5)*t(i))];
         end                            
         F=x(1) + x(2) * exp(-x(4)*t) + x(3) * exp(-x(5)*t) - y; 
-        k=k+1;                      %迭代次数加1  
-        if (k > itermax) break; end
+        k = k + 1;                      %迭代次数加1  
+        if (k > itermax) break; end %设置迭代上限
     end  
-    sigma=(sum((J'*F).^2))^(1/2);          
-    F=(sum(F.^2))^(1/2);                 %显示迭代次数，变量取值，停机表达式值，目标函数值  
+    sigma = (sum((J'*F).^2))^(1/2);          
+    norm = (sum(F.^2));                 %目标函数值  
 end
--- Exp 7/Problem 6/main.m
+++ Exp 7/Problem 6/main.m
@@ -3,5 +3,9 @@ clear all
 t = 10*(data(:,1)-1);
 X0 = [0.5; 1.5; -1; 0.01; 0.02]; %设置迭代的初始值
 y = data(:,2);
-[X1, norm, res, ef, out] = lsqnonlin(@nonlinearregression, X0, [], [], optimset('Algorithm','Levenberg-Marquardt'), t, y);
-[X2, F, iter] = guassnewtonv2(t, y, X0, 1e-2, 1000);
+[X1, norm1, res, ef, out] = lsqnonlin(@nonlinearregression, X0, [], [], optimset('Algorithm','Levenberg-Marquardt','TolFun',1e-6,'MaxIter',1000), t, y);
+[X2, norm2, iter] = guassnewtonv2(t, y, X0, 1e-6, 1000);
+%% 进行扰动，在观察结果的变化
+y1 = y - 0.05 + 0.10*rand(length(y),1);
+[X1_1, norm1_1, res_1, ef_1, out_1] = lsqnonlin(@nonlinearregression, X0, [], [], optimset('Algorithm','Levenberg-Marquardt','TolFun',1e-6,'MaxIter',1000), t, y1);
+[X2_1, norm2_1, iter_1] = guassnewtonv2(t, y1, X0, 1e-6, 1000);
