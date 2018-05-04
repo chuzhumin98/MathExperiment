2ef6a3cbbc08d1e03a031e037f63dceb38fc7912
chuzhumin98 <chuzhumin98@gmail.com>
2018-05-04 10:25:06 +0800

    complete coding for problem 4

--- Exp 9/Problem 4/goalfunction4.m
+++ Exp 9/Problem 4/goalfunction4.m
@@ -4,6 +4,9 @@
 
 function f = goalfunction4(x)
     priceA = 9; priceB = 15;
-    priceJia = 6; priceYi = 16; priceBing = 10;
+    priceJia = 6; 
+    %priceYi = 16; 
+    priceBing = 10;
+    priceYi = 13;
     f = priceJia * x(1) + priceYi * x(2) + priceBing * (x(3) + x(4)) - priceA * x(5) - priceB * x(6);
 end
